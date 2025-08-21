a9224d3bfa8b8fef3adfe54b083a22745cdeacb9
rokinosun <rokinosun@gmail.com>
2025-08-22 00:49:12 +0900

    update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755791017|15721577424';
+const CACHE_VERSION = '1755791332|224944451';
 /** @type {string} */
 const CACHE_PREFIX = 'ヴィヴィスタック-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
